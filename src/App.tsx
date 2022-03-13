e64c157c33d4f0fb66e7a29bcf799453191ed6b9
Bryan <thenhernandezb@gmail.com>
2022-03-12 22:33:25 -0400

    just work

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,65 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import React from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+enum O {
+	r = 'rock',
+	p = 'paper',
+	s = 'scissor',
+}
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+enum winState {
+	lose = 'lose',
+	win = 'win',
+	tie = 'tie',
+}
+
+function App() {
+	function pick(userOption: O): void {
+		const cpu = pickRandom()
+		const con = isWin(userOption, cpu)
+
+		alert(`
+    you pick is: ${userOption}
+    cpe pick is: ${cpu}
+    you are: ${con}`)
+	}
+
+	function isWin(pick: O, foo = pickRandom()) {
+		if (
+			(pick === O.r && foo === O.s) ||
+			(pick === O.p && foo === O.r) ||
+			(pick === O.s && foo === O.p)
+		) {
+			return winState.win
+		}
+		if (pick === foo) {
+			return winState.tie
+    }
+    else {
+			return winState.lose
+		}
+	}
+
+	function pickRandom() {
+		const rand = Math.floor(Math.random() * 3 + 1)
+		if (rand === 1) return O.r
+		if (rand === 2) return O.p
+		if (rand === 3) return O.s
+	}
+
+	return (
+		<div className='App'>
+			<button className='button-game green' onClick={() => pick(O.r)}>
+				🪨
+			</button>
+			<button className='button-game blue' onClick={() => pick(O.p)}>
+				📃
+			</button>
+			<button className='button-game red' onClick={() => pick(O.s)}>
+				✂️
+			</button>
+		</div>
+	)
 }
 
 export default App
